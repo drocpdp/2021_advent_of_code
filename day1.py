ecd872dbdc0cbaf3b16092b69c53076b0800c329
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-03 15:02:48 +0100

    day 2

--- day1.py
+++ day1.py
@@ -14,5 +14,20 @@ def day1():
             prev = m
     return increases
 
+def day1_2():
+    fp = open('./day1.txt', 'r')
+    measures = [int(l) for l in fp.readlines()]
+    fp.close()
 
-print(day1())
+    increases = 0
+    total = sum(measures[0:2])
+    L, R = 0, 2
+    while R + 1 < len(measures):
+        new_total = total + measures[R+1]
+        new_total -= measures[R-2]
+        if new_total > total:
+            increases += 1
+        R += 1
+    return increases
+
+print(day1_2())
