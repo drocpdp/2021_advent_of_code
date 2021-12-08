a9ec92c293ae2f59106300f2efa6f591d1681004
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-08 15:21:36 +0100

    day 4 complete (albeit unoptimized)

--- day4.py
+++ day4.py
@@ -22,7 +22,6 @@ def day4_1(boards, picks):
         for r, row in enumerate(board):
             for c, col in enumerate(row):
                 where[col].append((b,r,c))
-
     for pick in picks:
         for board,row,col in where[pick]:
             boards[board][row][col] *= -1
@@ -36,3 +35,37 @@ def day4_1(boards, picks):
                             unmarked += col
                 return unmarked * pick
     return
+
+def day4_2(boards, picks):
+    where = defaultdict(list)
+
+    # populate where{} (a board locator)
+    for b, board in enumerate(boards):
+        for r, row in enumerate(board):
+            for c, col in enumerate(row):
+                where[col].append((b,r,c))
+
+    winner = [False for board in boards]
+    
+    for pick in picks:
+        for board,row,col in where[pick]:
+            if boards[board][row][col] == 0:
+                boards[board][row][col] = -1
+            else:
+                boards[board][row][col] *= -1
+            if winner[board] is False:
+                if sum([True for val in boards[board][row] if val < 0]) == 5 or \
+                    sum([True for val in [boards[board][rrow][col] for rrow in range(5)] if val < 0]) \
+                        == len(boards[board][row]):
+                    #a winner
+                    winner[board] = True
+                    if all(winner): # if all now winners
+                        unmarked = 0
+                        for row in boards[board]:
+                            for col in row:
+                                if col >= 0:
+                                    unmarked += col
+                        return unmarked * pick
+
+    return
+
--- day4_test.py
+++ day4_test.py
@@ -1,5 +1,5 @@
 
-from day4 import day4_1
+from day4 import day4_1, day4_2
 from day_util import DayUtil
 
 picks = [79,9,13,43,53,51,40,47,56,27,0,14,33,60,61,36,72,48,83,42,10,86,41,75,16,80,15,93,95,45,68,96,84,11,85,63,18,31,35,74,71,91,39,88,55,6,21,12,58,29,69,37,44,98,89,78,17,64,59,76,54,30,65,82,28,50,32,77,66,24,1,70,92,23,8,49,38,73,94,26,22,34,97,25,87,19,57,7,2,3,46,67,90,62,20,5,52,99,81,4]
@@ -20,4 +20,5 @@ def _create_boards():
     return boards
 
 
-print(day4_1(_create_boards(), picks))
+#print(day4_1(_create_boards(), picks))
+print(day4_2(_create_boards(), picks))
