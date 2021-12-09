82bd354afe99fd04cbdc30b7a04e99c603ab50ab
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-09 15:29:22 +0100

    finished day 7 part 1, but lets refactor a bit to get rid of the static ranges and find more optimal solution

---
+++ day7.py
@@ -0,0 +1,22 @@
+from day_util import DayUtil
+from collections import Counter
+
+def extract_data():
+    data = [int(d) for d in DayUtil().open_file("day7")[0].split(',')]
+    #data = [16,1,2,0,4,2,7,1,2,14] # test data
+    return data
+
+def get_val_for_alignment_position(align_idx):
+    data = Counter(extract_data())
+    total = 0
+    for item in data:
+        total += abs(align_idx - item) * (data[item])
+    return total
+
+def day7():
+    min_value = float('inf')
+    for i in range(2000):
+        min_value = min(get_val_for_alignment_position(i), min_value)
+    print(min_value)
+
+day7()
