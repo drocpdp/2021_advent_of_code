8cd76f954cc8a2a80de6c2e725ba4b815f92e016
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-09 15:48:04 +0100

    day 7 part 2 solved. sum of numbers 1 to n == n*(n+1) / 2

--- day7.py
+++ day7.py
@@ -12,11 +12,19 @@ def get_val_for_alignment_position(align_idx, data):
         total += abs(align_idx - item) * (data[item])
     return total
 
+def get_val(align_idx, data):
+    total = 0
+    for item in data:
+        n = abs(align_idx - item)
+        n_n = n * (n+1) // 2
+        total += n_n * (data[item])
+    return total
+
 def day7():
     data = Counter(extract_data())
     min_value = float('inf')
     for i in range(max(data)):
-        min_value = min(get_val_for_alignment_position(i, data), min_value)
+        min_value = min(get_val(i, data), min_value)
     print(min_value)
 
 day7() #
