0ded0ce3db4249ac2a73763e6f0cb284357e91c2
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-13 16:25:12 +0100

    day 10 part 2 complete

--- day10.py
+++ day10.py
@@ -1,4 +1,5 @@
 from day_util import DayUtil
+from functools import reduce
 
 def day10():
     lines = DayUtil().open_file("day10")
@@ -28,4 +29,42 @@ def day10():
                     stack.pop()
     return total
 
-print(day10())
+def day10_2():
+    lines = DayUtil().open_file("day10")
+    key = {
+            "(" : ")",
+            "{" : "}", 
+            "[" : "]",
+            "<" : ">"
+    }
+    score = {
+            ")" : 1,
+            "]" : 2,
+            "}" : 3,
+            ">" : 4
+    }
+
+    totals = []
+
+    for line in lines:
+        stack = []
+        valid = True
+        for ch in line:
+            if ch in key:
+                stack.append(ch)
+            else:
+                if key[stack[-1]] != ch:
+                    valid = False
+                    break
+                else:
+                    stack.pop()
+        total = 0
+        if valid:
+            while stack:
+                total = (total * 5) + score[key[stack.pop()]]
+            totals.append(total)
+    totals.sort()
+    return totals[len(totals) // 2]
+
+#print(day10())
+print(day10_2())
