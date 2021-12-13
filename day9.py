80facf6c45ecb462381e1cb1358f05499bcc9d2d
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-13 15:36:26 +0100

    day 9 part 2 complete

--- day9.py
+++ day9.py
@@ -1,4 +1,7 @@
 from day_util import DayUtil
+from functools import reduce
+import heapq
+
 
 
 def day9():
@@ -15,6 +18,66 @@ def day9():
                 low_points += input[row][col] + 1
     print(low_points)
 
+def day9_2():
+    """
+    Utilize an iterative (but space-heavy) DFS and a heapq() to store length and keep track of top n
+    lengths of basins.
+    """
+    input = [[int(ch) for ch in row] for row in DayUtil().open_file("day9")]
+    visited = [[False for ch in row] for row in input]
+    
+    all_basins = []
+    basin, stack = [], []
 
+    for row in range(len(input)):
+        for col in range(len(input[0])):
+            stack.append((row,col))
+            while stack:
+                y,x = stack.pop()
+                if visited[y][x] is False and input[y][x] != 9:
+                    basin.append((y,x))
+                    visited[y][x] = True
+                    if input[y][x] == 9:
+                        visited[y][x] = True
+                for yy,xx in ((y+1,x),(y-1,x),(y,x+1),(y,x-1)):
+                    if 0 <= yy < len(input) and 0 <= xx < len(input[0]):
+                        if visited[yy][xx] is False and input[yy][xx] != 9:
+                            stack.append((yy,xx))
+            heapq.heappush(all_basins,len(basin))
+            basin = []
+    return reduce(lambda a,b: a*b, heapq.nlargest(3,all_basins))
 
-day9()
+#day9()
+print(day9_2())
+
+"""
+Could be a disjoint graph find/union problem.
+Instead of a list, use a dict for quicker lookup.
+NOTE: per requirements --> all other locations will always be part of only one basin.
+    So there will not be any "true peaks" within an area.
+
+parents = {}
+
+def find(node):
+    if node not in parents:
+        parents[node] = node
+        return node
+    if parents[node] == node:
+        return node
+    else:
+        return parents[node]
+    return find(parents[node])
+
+def union(fr, to):
+    parent_a = find(fr)
+    parent_b = find(to)
+    if parent_a == parent_a or parent_b == parent_b:
+        if parent_a == parent_a:
+            parents[parent_b] = parent_a
+        else:
+            parents[parent_a] = parent_b
+    else:
+        parents[parent_b] = parent_a
+    return
+"""
+
