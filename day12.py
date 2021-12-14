9562a0cba3a82195725a64cf9644683a03274c1f
David Eynon <davideynon@Davids-MacBook-Pro.local>
2021-12-14 15:59:44 +0100

    moving data to new data folder

---
+++ day12.py
@@ -0,0 +1,8 @@
+from day_util import DayUtil
+
+def day12():
+    data = DayUtil().open_file("day12")
+    print(data)
+
+
+day12()
--- day_util.py
+++ day_util.py
@@ -1,7 +1,7 @@
 class DayUtil:
 
     def open_file(self, day_str):
-        fp = open('{}.txt'.format(day_str), 'r')
+        fp = open('./data/{}.txt'.format(day_str), 'r')
         measures = [l.strip() for l in fp.readlines()]
         fp.close()           
         return measures
