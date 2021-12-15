9a9d81597165958962efd2729deef067fbaa7316
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-15 10:07:30 +0100

    day 12 part 1 complete

--- day12.py
+++ day12.py
@@ -1,8 +1,30 @@
 from day_util import DayUtil
+from collections import defaultdict
+
+def populate_graph():
+    graph = defaultdict(list)
+    data = DayUtil().open_file("day12")
+    for line in data:
+        fr, to = line.split("-")
+        graph[fr].append(to)
+        if fr != "start":
+            graph[to].append(fr)
+    return graph    
 
 def day12():
-    data = DayUtil().open_file("day12")
-    print(data)
+    """Find all paths graph algo with a twist"""
+    graph = populate_graph()
+    paths = []
+    stack = [["start"]]
+    while stack:
+        path = stack.pop()
+        if path[-1] == "end":
+            paths.append(path)
+        else:
+            for node in graph[path[-1]]:
+                if node.upper() == node or (node.lower() == node and node not in path):
+                    new_path = path + [node]
+                    stack.append(new_path)
+    return len(paths)
 
-
-day12()
+print(day12())
