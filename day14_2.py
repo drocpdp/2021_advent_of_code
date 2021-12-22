bf4af9de97bd40c6c3953a0ce14835b46736fcc3
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-22 20:08:13 +0100

    day 14 part 2 finished

--- day14_2.py
+++ day14_2.py
@@ -1,5 +1,5 @@
 from day_util import DayUtil
-from collections import Counter
+from collections import Counter, defaultdict
 
 class LL:
     def __init__(self, val=None):
@@ -7,41 +7,46 @@ class LL:
         self.val = val
 
 def get_data():
-    data = DayUtil().open_file("day14_test")
+    data = DayUtil().open_file("day14")
     initial = data[0]
-    template = {fr.strip():to.strip() for (fr,to) in [data[i].split("->") for i in range(2, len(data))]}
-    return (initial,template)
+    paths = {fr.strip():to.strip() for (fr,to) in [data[i].split("->") for i in range(2, len(data))]}
+    return (initial,paths)
 
-def testLL(head):
-    ll = []
-    node = head
-    while node:
-        ll.append(node.val)
-        node = node.next
-    print(ll)
-        
+def get_pairs_map(paths):
+    pairs = {}
+    for path in paths:
+        single = paths[path]
+        pairs[path] = [path[0]+single, single+path[1]]
+    return pairs
 
 def day14():
-    polymer, template = get_data()
-    head = LL(-1)
-    curr = head
-    for p in polymer:
-        curr.next = LL(p)
-        curr = curr.next
+    initial_polymer, graph = get_data()
+    pairs_map = get_pairs_map(graph)
+    pairs_count = Counter()
+    
+    [pairs_count.update([initial_polymer[i:i+2]]) for i in range(len(initial_polymer)-1)]
 
-    for _ in range(4):
-        ptr1 = head.next
-        ptr2 = ptr1.next
-        while ptr1.next:
-            insertion = LL(template[ptr1.val+ptr2.val])
-            ptr1.next = insertion
-            insertion.next = ptr2
-            ptr1 = ptr2
-            ptr2 = ptr1.next
-    testLL(head)
+    for _ in range(39):
+        tmp_pairs = pairs_count.copy()
+        for key in tmp_pairs:
+            cnt = tmp_pairs[key]
+            pair1, pair2 = pairs_map[key]
+            pairs_count[pair1] += cnt
+            pairs_count[pair2] += cnt
+            if pairs_count[key] > 0:
+                pairs_count[key] -= cnt
+    letters_count = defaultdict(int)
+
+    for pair in pairs_count:
+        cnt = pairs_count[pair]
+        pair1, pair2 = pairs_map[pair]
+        for letter in pair1:
+            letters_count[letter] = letters_count[letter] + cnt
+    letters_count[[initial_polymer][-1][-1]] += 1
+    
+    answer = max(letters_count.values()) - min(letters_count.values())
+    print(answer)
 
 
 
-
-
-print(day14())
+day14()
