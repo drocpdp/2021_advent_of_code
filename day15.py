10b1f82404d84992a35261bc846055050ad6f924
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-23 14:54:50 +0100

    day 15 part 1 fully complete

--- day15.py
+++ day15.py
@@ -2,26 +2,22 @@ from day_util import DayUtil
 from collections import deque
 
 def get_data():
-    data = DayUtil().open_file("day15_test")
+    data = DayUtil().open_file("day15")
     return [[int(col) for col in row] for row in data]
 
 
 def day15():
-    # djikstra algo
-    # first with DFS, then with priority queue if suboptimal
+    # If using BFS, len of deque gets too long. Switching to more of a DP solution
     data = get_data()
-    visited = [[False for col in row] for row in data]
     djikstra = [[float('inf') for col in row] for row in data]
     djikstra[0][0] = 0
-    dq = deque([(0,0)])
-    visited[0][0] = True
-    while dq:
-        y,x = dq.popleft()
-        visited[y][x] = True
-        for yy,xx in ((y+1,x),(y-1,x),(y,x+1),(y,x-1)):
-            if 0 <= yy < len(djikstra) and 0 <= xx < len(djikstra[0]) and visited[yy][xx] is False:
-                djikstra[yy][xx] = min(djikstra[y][x] + data[yy][xx], djikstra[yy][xx])
-                dq.append([yy,xx])
+
+    for row in range(len(djikstra)):
+        for col in range(len(djikstra[0])):
+            for rrow, ccol in ((row,col+1), (row+1, col)):
+                if rrow < len(data) and ccol < len(data[0]):
+                    djikstra[rrow][ccol] = min(djikstra[row][col] + data[rrow][ccol], djikstra[rrow][ccol])
     return djikstra[-1][-1]
+
 
 print(day15())
