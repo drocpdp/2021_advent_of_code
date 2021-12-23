a95e1b38e03c77a32d2ea88c9bfec32e58f2f134
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-23 14:16:37 +0100

    day 15 part 1 - small fix, discounting day 1 (position 0,0)

--- day15.py
+++ day15.py
@@ -2,7 +2,7 @@ from day_util import DayUtil
 from collections import deque
 
 def get_data():
-    data = DayUtil().open_file("day15")
+    data = DayUtil().open_file("day15_test")
     return [[int(col) for col in row] for row in data]
 
 
@@ -12,7 +12,7 @@ def day15():
     data = get_data()
     visited = [[False for col in row] for row in data]
     djikstra = [[float('inf') for col in row] for row in data]
-    djikstra[0][0] = data[0][0]
+    djikstra[0][0] = 0
     dq = deque([(0,0)])
     visited[0][0] = True
     while dq:
@@ -22,6 +22,6 @@ def day15():
             if 0 <= yy < len(djikstra) and 0 <= xx < len(djikstra[0]) and visited[yy][xx] is False:
                 djikstra[yy][xx] = min(djikstra[y][x] + data[yy][xx], djikstra[yy][xx])
                 dq.append([yy,xx])
-    return djikstra[-1][-1] - 1
+    return djikstra[-1][-1]
 
 print(day15())
