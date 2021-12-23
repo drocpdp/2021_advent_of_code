2ba545b0c36278f9507b3992d528033a30cbef1e
David Eynon <davideynon@davids-macbook-pro.home>
2021-12-23 17:06:43 +0100

    day 15 part 2 almost complete

--- day15.py
+++ day15.py
@@ -19,5 +19,35 @@ def day15():
                     djikstra[rrow][ccol] = min(djikstra[row][col] + data[rrow][ccol], djikstra[rrow][ccol])
     return djikstra[-1][-1]
 
+def transposed_data(rrow, ccol, data):
+    root_row = rrow % len(data)
+    root_col = ccol % len(data[0])
+    root_value = data[root_row][root_col]
+    multiplier = (rrow // len(data)) + (ccol // len(data[0]))
+    new_value = (root_value + multiplier)
+    if new_value > 9:
+        new_value = new_value % 10 + (new_value // 10)
+    return new_value
 
-print(day15())
+
+
+def day15_2():
+    data = get_data()
+    height = len(data) * 5
+    length = len(data[0]) * 5
+
+    djikstra = [[float('inf') for col in range(length)] for row in range(height)]
+    djikstra[0][0] = 0
+
+    for row in range(height):
+        for col in range(length):
+            for rrow, ccol in ((row,col+1), (row+1, col)):
+                if rrow < height and ccol < length:
+                    djikstra[rrow][ccol] = min(
+                        djikstra[row][col] + transposed_data(rrow, ccol, data),
+                        djikstra[rrow][ccol]
+                        )
+    return djikstra[-1][-1]
+
+#print(day15())
+print(day15_2())
