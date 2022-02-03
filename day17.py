454bf5520b3773fe24ba745315bda759846b1af3
David Eynon <davideynon@davids-macbook-pro.home>
2022-02-03 16:11:40 +0100

    day 17 - setup

--- day17.py
+++ day17.py
@@ -1,15 +1,17 @@
 from day_util import DayUtil
+from itertools import product
 
 isTest = True
 
 def get_data():
-    file_str = "day17_test" if isTest else "day17"
-    data = DayUtil().open_file(file_str)
+    target_points_real = product((i for i in range(169,206+1)), (j for j in range(-108,-68+1))) #full
+    target_points_test = product((i for i in range(20,30+1)), (j for j in range(-10,-5+1))) #test    
+    data = target_points_test if isTest else target_points_real
     return data
 
 def day17():
     data = get_data()
-    print(data)
+    print(list(data))
 
 
 day17()
